dfe1598a1f6269d1ed4e41ecc7decb38c1158afb
Tim Quinlan <timothyquinlan1983@gmail.com>
2021-05-06 23:15:47 -0500

    fixed mysql link

--- api/lib/db.js
+++ api/lib/db.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 // You can choose to connect to sqlite here if you wish to
 // Also, if you are using mysql on your local machine you need to update the credentials below
-const sequelize = new Sequelize(process.env.CLEARDB_DATABASE_URL || 'mysql://root:password@localhost/cohort12a-capstone-api', { logging: false });
+const sequelize = new Sequelize(process.env.CLEARDB_DATABASE_URL || 'mysql://root:password@localhost/capstone-api', { logging: false });
 
 //Make sure you run this: npm install sqlite3 --save
 // const sequelize = new Sequelize({
